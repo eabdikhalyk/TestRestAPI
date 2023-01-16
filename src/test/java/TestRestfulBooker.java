8a3d0b9167afd778cd1484c02dda5f2188b1eaa1
eabdikhalyk <eabdikhalyk@mail.ru>
2023-01-16 17:26:55 +0500

    Added post method for getting Token

--- src/test/java/TestRestfulBooker.java
+++ src/test/java/TestRestfulBooker.java
@@ -1,11 +1,47 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
 
 import static io.restassured.RestAssured.given;
 
 public class TestRestfulBooker {
-    String url = "https://restful-booker.herokuapp.com/booking";
+    String url = "https://restful-booker.herokuapp.com";
+    String contentType = "Content-type";
+    String appJson = "application/json";
+    String requestBody = "{ " +
+    " \"username\" : \"admin\" ,"
+          + "  \"password\" : \"password123\"}";
+    @BeforeTest
+    public void setup(){
+        RestAssured.baseURI = url;
+    }
     @Test
     public void getAllBookings(){
-        given().when().get(url).then().log().all();
+        Response response = given().
+                when().
+                get(url).
+                then().
+                extract().
+                response();
+        Assert.assertEquals(response.statusCode(),200);
+    }
+
+    @Test
+    public void createToken(){
+        Response response = given().
+                header(contentType, appJson).
+                and().
+                body(requestBody).
+                when().
+                post("/auth").
+                then().
+                extract().
+                response();
+
+        Assert.assertEquals(response.statusCode(),200);
+
     }
 }
